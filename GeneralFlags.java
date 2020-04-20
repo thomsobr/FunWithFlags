e4df5e1f81b53d447ff3ca0b57c8511077012c01
Brook <brook.thomson@student.onslow.school.nz>
2020-04-21 11:43:53 +1200

    Changed from arraylist inputs to multiple int inputs as the color class does not take doubles. Created first, second and last colors. Began work for drawing a horizontal flag. Code is currently very chunky and long and in one method. Will need to break everything down into smaller bits and streamline later.

--- GeneralFlags.java
+++ GeneralFlags.java
@@ -15,6 +15,8 @@ import java.awt.Color;
  * @version 21/04/20
  */
 public class GeneralFlags{
+    public static final double X = 100;
+    public static final double Y = 100;
 
     /**
      * Constructor for objects of class GeneralFlags 
@@ -25,12 +27,21 @@ public class GeneralFlags{
     }
     
     public void getInfo(){
-        ArrayList<Double> firstColor = UI.askNumbers("Enter RGB values for the"
-        + " top/leftmost stripe: ");
-        ArrayList<Double> middleColor = UI.askNumbers("Enter RGB values for the"
-        + " middle stripe: ");
-         ArrayList<Double> lastColor = UI.askNumbers("Enter RGB values for the"
-        + " last stripe: ");
+        int firstR = UI.askInt("Enter RGB values for the"
+        + " top/leftmost stripe. R: ");
+        int firstG = UI.askInt("G: ");
+        int firstB = UI.askInt("B: ");
+        Color first = new Color(firstR, firstG, firstB);
+        int secondR = UI.askInt("Enter RGB values for the"
+        + " middle stripe. R: ");
+        int secondG = UI.askInt("G: ");
+        int secondB = UI.askInt("B: ");
+        Color second = new Color(secondR, secondG, secondB);
+        int lastR = UI.askInt("Enter RGB values for the"
+        + " last stripe. R: ");
+        int lastG = UI.askInt("G: ");
+        int lastB = UI.askInt("B: ");
+        Color last = new Color(lastR, lastG, lastB);
         double vert = UI.askDouble("Ratio for height (ie if ratio is 1:2, enter 1): ");
         double hori = UI.askDouble("Ratio for width: ");
         String direction = UI.askString("Are the stripes on the flag horizontal "
@@ -38,11 +49,22 @@ public class GeneralFlags{
         if (direction == "Horizontal") {
             double stripeLength = vert * 50 / 3;
             double stripeWidth = hori * 50;
+            double bottom = Y + vert * 50;
+            double middle = bottom - stripeLength;
+            double top = middle - stripeLength;
+            UI.setLineWidth(1);
+            UI.setColor(last);
+            UI.fillRect(X, bottom, stripeWidth, stripeLength);
+            UI.setColor(second);
+            UI.fillRect(X, middle, stripeWidth, stripeLength);
+            UI.setColor(first);
+            UI.fillRect(X, top, stripeWidth, stripeLength);
         }
         else {
             double stripeLength = vert * 50;
             double stripeWidth = hori * 50 /3;
         }
+        
         
     }
 
