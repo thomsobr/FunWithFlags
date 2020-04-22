1b1c1f202c01e7e6470523d5934e992afc2f6d89
Brook <brook.thomson@student.onslow.school.nz>
2020-04-22 14:17:51 +1200

    Code now successfully makes vertical and horizontal flags. Have now created a method to create new colours so I don't have lots of repeating code.

--- GeneralFlags.java
+++ GeneralFlags.java
@@ -27,33 +27,21 @@ public class GeneralFlags{
     }
     
     public void getInfo(){
-        int firstR = UI.askInt("Enter RGB values for the"
-        + " top/leftmost stripe. R: ");
-        int firstG = UI.askInt("G: ");
-        int firstB = UI.askInt("B: ");
-        Color first = new Color(firstR, firstG, firstB);
-        int secondR = UI.askInt("Enter RGB values for the"
-        + " middle stripe. R: ");
-        int secondG = UI.askInt("G: ");
-        int secondB = UI.askInt("B: ");
-        Color second = new Color(secondR, secondG, secondB);
-        int lastR = UI.askInt("Enter RGB values for the"
-        + " last stripe. R: ");
-        int lastG = UI.askInt("G: ");
-        int lastB = UI.askInt("B: ");
-        Color last = new Color(lastR, lastG, lastB);
+        Color first = makeColor();
+        Color second = makeColor();
+        Color last = makeColor();
         double vert = UI.askDouble("Ratio for height (ie if ratio is 1:2, enter 1): ");
         double hori = UI.askDouble("Ratio for width: ");
         String direction = UI.askString("Are the stripes on the flag horizontal "
         + "or vertical?" );
+        UI.setLineWidth(1);
+        UI.setColor(last);
         if (direction == "Horizontal") {
             double stripeLength = vert * 50 / 3;
             double stripeWidth = hori * 50;
             double bottom = Y + vert * 50;
             double middle = bottom - stripeLength;
             double top = middle - stripeLength;
-            UI.setLineWidth(1);
-            UI.setColor(last);
             UI.fillRect(X, bottom, stripeWidth, stripeLength);
             UI.setColor(second);
             UI.fillRect(X, middle, stripeWidth, stripeLength);
@@ -63,9 +51,25 @@ public class GeneralFlags{
         else {
             double stripeLength = vert * 50;
             double stripeWidth = hori * 50 /3;
+            double middle = X + stripeWidth;
+            double lastStripe = middle + stripeWidth;
+            UI.fillRect(lastStripe, Y, stripeWidth, stripeLength);
+            UI.setColor(second);
+            UI.fillRect(middle, Y, stripeWidth, stripeLength);
+            UI.setColor(first);
+            UI.fillRect(X, Y, stripeWidth, stripeLength);
         }
         
         
+    }
+    
+    public Color makeColor() {
+        int firstR = UI.askInt("Enter RGB values for the"
+        + " top/leftmost stripe. R: ");
+        int firstG = UI.askInt("G: ");
+        int firstB = UI.askInt("B: ");
+        Color col = new Color(firstR, firstG, firstB);
+        return col;
     }
 
 
